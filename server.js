360e883ab6d9bbc8b7d94374de23af9d9c616df0
Brad Schoonover <badonmap@WIN2.CS.KSU.EDU>
2017-01-31 14:25:22 -0600

    added css page and html variable

--- server.js
+++ server.js
@@ -9,6 +9,8 @@
 var http = require("http");
 var fs = require('fs');
 var port = 5000;
+var stylesheet = fs.readFileSync('gallery.css')
+var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg']
 
 /*
 use to start the server when having small files not thousands of fileSize
@@ -38,45 +40,68 @@ var server = http.createServer(function(req, res) {
 	
 	switch(req.url)
 	{
+		case '/gallery':
+			var gHtml = imageNames.map(function(fileName){
+				return ' <img src ="' + fileName + '"alt = "a fishing ace at work">';
+			}).join('');
+			var html = "<!doctype html><head>";
+				html += "<head>";
+				html +=  "<title>Gallery</title>"
+				html +=  '<link href="gallery.css" rel="stylesheet" type="text/css">'
+				html += "</head>";
+				html += "<body>";
+				html += " <h1>Gallery</h1>";
+				html += "<h2> Hello. </h2> Time is " + Date.now();
+				html += gHtml
+				html += "</body>";
+			res.setHeader('content-Type', 'text/html')
+			res.end(html)
+			break;
+
 		case "/bubble":
-		case "bubble/":
-		case "bubble.jpg":
-		case "bubble.jpeg":
-			serveImage('bubble.jpg', req, res)
+		case "/bubble/":
+		case "/bubble.jpg":
+		case "/bubble.jpeg":
+			serveImage('bubble.jpg', req, res);
 			break;
 			
 		case "/chess":
-		case "chess/":
-		case "chess.jpg":
-		case "chess.jpeg":
-			serveImage('chess.jpg', req, res)
+		case "/chess/":
+		case "/chess.jpg":
+		case "/chess.jpeg":
+			serveImage('chess.jpg', req, res);
 			break;
 			
 		case "/ace":
-		case "ace/":
-		case "ace.jpg":
-		case "ace.jpeg":
-			serveImage('ace.jpg', req, res)
+		case "/ace/":
+		case "/ace.jpg":
+		case "/ace.jpeg":
+			serveImage('ace.jpg', req, res);
 			break;
 			
 		case "/fern":
-		case "fern/":
-		case "fern.jpg":
-		case "fern.jpeg":
-			serveImage('fern.jpg', req, res)
+		case "/fern/":
+		case "/fern.jpg":
+		case "/fern.jpeg":
+			serveImage('fern.jpg', req, res);
 			break;
 			
 		case "/mobile":
-		case "mobile/":
-		case "mobile.jpg":
-		case "mobile.jpeg":
-			serveImage('mobile.jpg', req, res)
+		case "/mobile/":
+		case "/mobile.jpg":
+		case "/mobile.jpeg":
+			serveImage('mobile.jpg', req, res);
 			break;
-		
+			
+		case "/gallery.css":
+			res.setHeader('Content-Type', 'text/css');
+			res.end(stylesheet);
+			break;
 		default:
 			res.statusCode = 404;
 			res.statusMessage = "Not Found";
-			res.end("Not Found")
+			res.end("Not Found");
+			break;
 	}		
 	
 });
